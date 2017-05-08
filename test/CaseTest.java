667da6d14bd4c64d03fe40c18e8db444e8f85d11
Roman Volosatovs <rvolosatovs@riseup.net>
2017-05-08 17:03:42 +0200

    PackingSolverTest->CaseTest, refactor and fix

---
+++ test/CaseTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by s154563 on 3-5-2017.
+ */
+public class CaseTest {
+    @Test
+    public void readAllFilesTest() {
+        try {
+            Stream<Path> paths = Files.walk(Paths.get("test/cases"));
+            paths.forEach(path -> {
+                if (Files.isRegularFile(path)) {
+                    try {
+                        System.out.println("Parsing " + path.toString());
+                        new Case(new FileInputStream(path.toFile()));
+                    } catch (Exception e) {
+                        fail(e.getMessage());
+                    }
+                }
+            });
+            paths.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
