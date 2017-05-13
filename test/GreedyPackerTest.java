a347e66058eff126a41dc45a763f77c3cb66014a
Roman Volosatovs <rvolosatovs@riseup.net>
2017-05-13 20:32:54 +0200

    test: remove isValidCOntainer

--- test/GreedyPackerTest.java
+++ test/GreedyPackerTest.java
@@ -30,36 +30,6 @@ public class GreedyPackerTest extends PackerTest {
     public void testAllFiles() { super.testAllFiles(); }
 
     @Test
-    public void testIsValidContainer() {
-        GreedyPacker greedy = new GreedyPacker();
-        Dimension[] dimensions = new Dimension[1];
-        dimensions[0] = new Dimension(2, 3);
-        //dimensions[1] = new Dimension(5, 7);
-        //dimensions[2] = new Dimension(2, 4);
-        int containerHeight = 6;
-        Case c = new Case(containerHeight, true, dimensions);
-        ArrayList<IndexedRectangle> rectangles = (ArrayList<IndexedRectangle>)c.getRectangles();
-        rectangles.get(0).setLocation(0, 3);
-        assertTrue(greedy.isValidContainer(new Container(rectangles), c));
-    }
-
-    @Test
-    public void testIsValidContainer1() {
-        GreedyPacker greedy = new GreedyPacker();
-        Dimension[] dimensions = new Dimension[1];
-        dimensions[0] = new Dimension(2, 3);
-        //dimensions[1] = new Dimension(5, 7);
-        //dimensions[2] = new Dimension(2, 4);
-        int containerHeight = 6;
-        Case c = new Case(containerHeight, true, dimensions);
-        ArrayList<IndexedRectangle> rectangles = (ArrayList<IndexedRectangle>)c.getRectangles();
-        //System.out.println(rectangles);
-        rectangles.get(0).setLocation(0, 6);
-        //System.out.println(rectangles);
-        assertFalse(greedy.isValidContainer(new Container(rectangles), c));
-    }
-
-    @Test
     public void testPointsAvailable1() {
         GreedyPacker greedy = new GreedyPacker();
         Dimension[] dimensions = new Dimension[1];
