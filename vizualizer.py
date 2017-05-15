b0a884eacf7d7482ae983b5029352cc83e2096ee
Luuc Verburgh <luuc.verburgh@gmail.com>
2017-05-15 20:46:48 +0200

    parser works correctly

--- vizualizer.py
+++ vizualizer.py
@@ -1,19 +1,42 @@
 import sys
 
-class Input:
-	def __init__(self):
+class InputParser:
+	def __init__(self, input):
 		self.heightIsFixed = 0
 		self.height = 0
 		self.numRectangles = 0
+		self.positions = []
+		self.sizes = []
 
-print("please copy and paste your charge discharge data.")
-        
+		self.getPositions(input)
+		self.getSizes(input)
+
+
+	def getPositions(self, lines):
+		intStart = lines.find('placement of rectangles') + len('placement of rectangles\n')
+		positions = lines[intStart:].split('\n')
+
+		for p in positions:
+			self.positions.append(p.split(' '))
+
+	def getSizes(self, lines):
+		inputInts = lines[lines.find('number of rectangles:')+len('number of rectangles: '):lines.find('\nplacement of rectangles')].split('\n')
+		
+		self.numRectangles = inputInts[0]
+		
+		sizes = []
+		for i in inputInts:
+			sizes.append(i.split(' '))
+		sizes.pop(0)
+		self.sizes = sizes
+
+
+	
+
+print("paste output, control d when done")        
 lines = sys.stdin.read()
-print lines
-#end = lines.find('placement of rectangles') + len('placement of rectangles')
-#print end
-#print lines[end:]
-#input = Input()
-#input.find= lines[1]
+input = InputParser(lines)
 
-#print input.heightIsFixed
+print input.numRectangles
+print input.sizes
+print input.positions
