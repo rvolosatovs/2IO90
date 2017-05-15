961a1a7ef3da1704178715f478775ecc64d9aee4
Luuc Verburgh <luuc.verburgh@gmail.com>
2017-05-15 16:30:20 +0200

    progress

--- vizualizer.py
+++ vizualizer.py
@@ -1,18 +1,19 @@
+import sys
+
 class Input:
 	def __init__(self):
 		self.heightIsFixed = 0
 		self.height = 0
 		self.numRectangles = 0
 
-print("please copy and paste your charge discharge data.\n"
-      "To end recording Press Ctrl+d on Linux/Mac on Crtl+z on Windows")
-lines = []
+print("please copy and paste your charge discharge data.")
         
-lines.append(raw_input())
-
-end = lines.find('placement of rectangles') + len('placement of rectangles')
-
-input = Input()
-input.find= lines[1]
+lines = sys.stdin.read()
+print lines
+#end = lines.find('placement of rectangles') + len('placement of rectangles')
+#print end
+#print lines[end:]
+#input = Input()
+#input.find= lines[1]
 
 #print input.heightIsFixed
