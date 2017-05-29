e1542ad02076b88802f58f3c18388f39e738d05f
Luuc Verburgh <luuc.verburgh@gmail.com>
2017-05-29 15:48:02 +0200

    finalized some stuff

--- src/NFDHPacker.java
+++ src/NFDHPacker.java
@@ -7,52 +7,40 @@ import java.lang.*;
  *  Next fit decreasing height packing
  *  Only use this when fixed height.
  *
- *  In sorted on height order; place rectangle with biggest height in left bottom
+ *  In sorted on width order; place rectangle with biggest height in left bottom
  *  then if fits, add next rectangle on top, if not, place it against new 'wall',
- *  the max x position of the biggest regtangle in previous row.
+ *  the max x position of the biggest rectangle in previous row.
+ *
  *
  */
 public class NFDHPacker implements Packer {
 
-    /**
-     * credits to Mr. Jolan
-     * Sorts a Collection of IndexedRectangles on their height.
-     * @param rectangles
-     * @return Sorted descending ArrayList on height with the rectangles
-     */
     private ArrayList<IndexedRectangle> sortOnHeight(Collection<IndexedRectangle> rectangles, Case c) {
         ArrayList<IndexedRectangle> result = new ArrayList<>();
 
         for (IndexedRectangle rectangle: rectangles) {
             if (c.areRotationsAllowed()) {
-                System.out.println("\n");
-                System.out.println("width: "+ rectangle.width);
                 if (rectangle.height > rectangle.width) {
                     if (c.getHeight() > rectangle.height) {
                         rectangle.rotate();
                     }
                 }
                 if (rectangle.height > c.getHeight()){
-                    System.out.println("swapped"+ rectangle.height +" "+rectangle.width);
                     rectangle.rotate();
                 }
-                System.out.println("width: "+ rectangle.width);
             }
 
             if (result.isEmpty()) {
-                System.out.println("vvvv");
                 result.add(rectangle);
             } else {
                 int width = rectangle.width;
                 for (int i = 0; i < result.size(); i++) {
                     if (width >= result.get(i).width) {
                         result.add(i, rectangle);
-                        System.out.println("vvvfvffvvffvfvvv");
                         break;
                     }
                 }
                 if (!result.contains(rectangle)) {
-                    System.out.println("vzzzzzzz");
                     result.add(rectangle);
                 }
             }
