8899c11274d25d67c64078b4c7ce0a2ecbb41053
Luuc Verburgh <luuc.verburgh@gmail.com>
2017-05-31 10:54:00 +0200

    works for all test cases with fixed height

--- src/NFDHPacker.java
+++ src/NFDHPacker.java
@@ -2,6 +2,7 @@ import java.util.Collection;
 import java.util.*;
 import java.awt.*;
 import java.lang.*;
+import java.util.List;
 
 /**
  *  Next fit decreasing height packing
@@ -11,54 +12,19 @@ import java.lang.*;
  *  then if fits, add next rectangle on top, if not, place it against new 'wall',
  *  the max x position of the biggest rectangle in previous row.
  *
- *
  */
 public class NFDHPacker implements Packer {
-
-    private ArrayList<IndexedRectangle> sortOnHeight(Collection<IndexedRectangle> rectangles, Case c) {
-        ArrayList<IndexedRectangle> result = new ArrayList<>();
-
-        for (IndexedRectangle rectangle: rectangles) {
-            if (c.areRotationsAllowed()) {
-                if (rectangle.height > rectangle.width) {
-                    if (c.getHeight() > rectangle.height) {
-                        rectangle.rotate();
-                    }
-                }
-                if (rectangle.height > c.getHeight()){
-                    rectangle.rotate();
-                }
-            }
-
-            if (result.isEmpty()) {
-                result.add(rectangle);
-            } else {
-                int width = rectangle.width;
-                for (int i = 0; i < result.size(); i++) {
-                    if (width >= result.get(i).width) {
-                        result.add(i, rectangle);
-                        break;
-                    }
-                }
-                if (!result.contains(rectangle)) {
-                    result.add(rectangle);
-                }
-            }
-        }
-        return result;
-    }
-
+    
     public Container Pack(Case c) {
-        Collection<IndexedRectangle> originalRectangles = c.getRectangles();
-
-        ArrayList<IndexedRectangle> sortedRectangles = sortOnHeight(originalRectangles, c);
+        List<IndexedRectangle> rectangles = c.getRectangles();
+        Util.sortByWidth(rectangles, c);
 
         int y = 0;
         int potWall = 0;
         int wall = 0;
 
         if (c.isHeightFixed()) {
-            for (IndexedRectangle r : sortedRectangles) {
+            for (IndexedRectangle r : rectangles) {
 
                 if(y + r.height > c.getHeight()){
                     wall = potWall;
@@ -72,7 +38,7 @@ public class NFDHPacker implements Packer {
 
             }
         }
-        return new Container(sortedRectangles);
+        return new Container(rectangles);
 
     }
 }
