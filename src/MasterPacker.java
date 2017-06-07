1cb36a4856f44c42860b20c39d5daba484d48942
Berrie Trippe <berrietrippe@gmail.com>
2017-06-07 11:21:19 +0200

    MasterPacker: add default algorithm switch operation

---
+++ src/MasterPacker.java
@@ -0,0 +1,17 @@
+/**
+ * Created by berrietrippe on 07/06/2017.
+ */
+public class MasterPacker implements Packer {
+    @Override
+    public Container Pack(Case c) {
+        int caseSize = c.getSize();
+        switch(caseSize){
+            case 3: return (new GreedyPacker().Pack(c));
+            case 5: return (new GreedyPacker().Pack(c));
+            case 10: return (new GreedyPacker().Pack(c));
+            case 25: return (new GreedyPacker().Pack(c));
+            case 10000: return (new GreedyPacker().Pack(c));
+            default: return (new GreedyPacker().Pack(c));
+        }
+    }
+}
